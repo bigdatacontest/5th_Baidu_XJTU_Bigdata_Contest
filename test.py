97dc559bef02c056bf87bc2d42490b7914e2a88b
pengjizhi <49836762@qq.com>
2019-05-17 13:29:42 +0800

    重命名了transform,分别是train和test

--- test.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a = np.loadtxt('a.txt')
-print(a)
--- transform_test.py
+++ transform_test.py
@@ -1,8 +1,8 @@
 """ 
 1.读取train_visit/train的每一个文件  os.dir
-2.f.open(),打开每一个文件，每个文件计算出自己的矩阵。存为一个npy
-3.多个npy压缩成一个npz?
-4.怎么拼接成4维？
+2.打开每一个文件，每个文件计算出自己的矩阵。存为一个txt或则npy
+3.需不需要压缩成一个文件？
+4.读取数据的时候，怎么拼接，占内存会不会很大
 """
 import os
 import numpy as np
@@ -11,12 +11,12 @@ import datetime
 
 origin_date = datetime.date(year=2018,month=10,day=1)
 # print(origin_date)
-path = '../data/test_visit/test/'
+path = '../data/test_visit/test/' # 没有就自己建
 
-target_array = np.zeros((4368))
 # print(target_array.shape) 
-dict_strdate = {}
-dict_all_date = {} # '20181001': 0, '20181002': 1, '20181003': 2, 
+dict_strdate = {} # 这里没用到，可能后面有用，为了速度快
+dict_all_date = {} # '20181001': 0, '20181002': 1, '20181003': 2', 用字典来读取，速度快
+
 for i in range(182):
     date = origin_date + datetime.timedelta(days=i)
     date = str(date).replace('-', '')
@@ -34,6 +34,9 @@ def getfilenames(path):
     return files
 
 def get_array_this_visit(date_hour):
+    """
+    每一个访问者，都对应自己的一个矩阵
+    """
     date_hour = date_hour.split(',')
     array_this_visit = np.zeros((4368))
     for item in date_hour:
@@ -55,13 +58,14 @@ filenames = getfilenames(path)
 
 for filename in filenames:
     file_path = path + filename
+    target_array = np.zeros((4368)) # 关键是要找到这个矩阵的索引，这个是目标矩阵
     content = pd.read_csv(file_path, sep='\t', usecols=[1])
     # print(len(content))
     for i in range(len(content)):
         # print(content.iloc[i][0])
-        target_array += get_array_this_visit(content.iloc[i][0])
+        target_array += get_array_this_visit(content.iloc[i][0]) # 目标矩阵找到在一个地区中，每一个访问者的矩阵并加起来
     
-    np.savetxt('../data/transform/test/'+filename, target_array, fmt='%d')
+    np.savetxt('../data/transform/test/'+filename, target_array, fmt='%d') # 一个地区保存为一个txt
 
 # print(target_array)
 
